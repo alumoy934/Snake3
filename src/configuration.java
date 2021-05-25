01a1759bbc997f8d73784ead0277ccb5c91e0182
carmoy <alumoy934@ieselcaminas.org>
2021-05-25 15:13:35 +0200

    Snake2

---
+++ src/configuration.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class configuration {
+    private JPanel panel1;
+    private JTextField username;
+    private JButton buttoneasy;
+    private JButton buttonmedium;
+    private JButton buttonhard;
+    private JLabel labelusername;
+    private JLabel level;
+}
